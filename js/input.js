4cd968400cf100194f389705e4d319f77d4977de
zhang_jie <1749862361@qq.com>
2019-02-03 23:46:33 +0800

    修改

--- js/input.js
+++ js/input.js
@@ -1,5 +1,5 @@
 function click(id) {
-	var butObj = document.getElementById("but001");
+	var butObj = document.getElementById(id);
 	butObj.style.backgroundColor = "red";
 	butObj.value = "哈哈啊哈";
 }
